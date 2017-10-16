faa36a96115825ab45d974f569a39f0485cad451
xuqinqing <xuqinqing@aiyaapp.com>
2017-10-16 19:12:15 +0800

    v1.0_1016_001

--- home/home_scrapy/home_scrapy/pipelines.py
+++ home/home_scrapy/home_scrapy/pipelines.py
@@ -6,10 +6,8 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
 import threading
-# from home_scrapy.DB_connection_pool import getdbconnectPool
 import pymysql
 from DBUtils.PooledDB import PooledDB
-from scrapy.utils.project import get_project_settings
 class HomeScrapyPipeline(object):
     def __init__(self):
         self.f = open("data.json", 'wb')
@@ -31,9 +29,7 @@ class HomeScrapyMySQLPipeline(object):
 
     @classmethod
     def from_settings(cls, settings):
-        '''1、@classmethod声明一个类方法，而对于平常我们见到的则叫做实例方法。
-           2、类方法的第一个参数cls（class的缩写，指这个类本身），而实例方法的第一个参数是self，表示该类的一个实例
-           3、可以通过类来调用，就像C.f()，相当于java中的静态方法'''
+        #  创建数据库连接池类方法
         dbpool = PooledDB(creator=pymysql,
                  mincached=settings['DB_MIN_CACHED'], maxcached=settings['DB_MAX_CACHED'],
                  maxshared=settings['DB_MAX_SHARED'], maxconnections=settings['DB_MAX_CONNECTIONS'],
@@ -42,18 +38,19 @@ class HomeScrapyMySQLPipeline(object):
                  user=settings['DB_USER'], passwd=settings['DB_PASSWD'],
                  db=settings['DB_NAME'], charset=settings['DB_CHARSET'], use_unicode=False
                  )
-        dbpool = dbpool.connection()
-        return cls(dbpool)  # 相当于dbpool付给了这个类，self中可以得到
+        return cls(dbpool)
 
     # pipeline默认调用
     def process_item(self, item, spider):
-            db = self.dbpool
-            cursor = db.cursor()
-            sql = "insert into area(id, aname, lv, pid) values(%s,%s,%s,%s)"
-            params = (item["id"], item["aname"], item["lv"], item["pid"])
-            cursor.execute(sql, params)
-            db.commit()
-            cursor.close()
-            db.close()
-            return item
+        lock.acquire()
+        conn = self.dbpool.connection()
+        cursor = conn.cursor()
+        sql = "insert into area(id, aname, lv, pid) values(%s,%s,%s,%s)"
+        params = (item["id"], item["aname"], item["lv"], item["pid"])
+        cursor.execute(sql, params)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        lock.release()
+        return item
 
--- home/home_scrapy/home_scrapy/spiders/spiderone.py
+++ home/home_scrapy/home_scrapy/spiders/spiderone.py
@@ -8,43 +8,42 @@ class SpideroneSpider(scrapy.Spider):
     name = 'spiderone'
     allowed_domains = ['http://www.stats.gov.cn']
     baseUrl = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/'
-    # start_urls = ['http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/index.html']
-    start_urls = ['http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/43.html']
+    start_urls = ['http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/index.html']
+    # start_urls = ['http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/43.html']
 
     # 省
-    # def parse(self, response):
-    #     node_list = response.xpath('//tr[@class="provincetr"]/td/a')
-    #     for province_node in node_list:
-    #         province_data = HomeScrapyItem()
-    #         aname = province_node.xpath('text()').extract()
-    #         id = province_node.xpath('td[1]/a/text()').extract()
-    #         url = province_node.xpath('@href').extract()
-    #         province_data['aname'] = aname[0]
-    #         province_data['id'] = id[0]
-    #         province_data['lv'] = 2
-    #         province_data['pid'] = 0
-    #         city_url = self.baseUrl + str(url[0])
-    #         # yield scrapy.Request(city_url, meta={'province_data': province_data}, callback=self.city_parse,
-    #         #                      encoding='utf-8', dont_filter=True)
-    #         yield province_data
-    # 市
-
     def parse(self, response):
+        node_list = response.xpath('//tr[@class="provincetr"]/td/a')
+        for province_node in node_list:
+            province_data = HomeScrapyItem()
+            aname = province_node.xpath('text()').extract()
+            url = province_node.xpath('@href').extract()
+            pre_id = str(url[0])
+            id = pre_id.split('.', -1)[0] + '0000000000'
+            province_data['aname'] = aname[0]
+            province_data['id'] = id
+            province_data['lv'] = 1
+            province_data['pid'] = 100000000000
+            city_url = self.baseUrl + str(url[0])
+            yield scrapy.Request(city_url, meta={'province_data': province_data}, callback=self.city_parse,
+                                 encoding='utf-8', dont_filter=True)
+            yield province_data
+
+    def city_parse(self, response):
         city_list = response.xpath('//tr[@class="citytr"]')
-        city_data = HomeScrapyItem()
+        meta = response.meta['province_data']
         for city_node in city_list:
-            # city_data = dict(meta)
+            city_data = dict(meta)
             number = city_node.xpath('td[1]/a/text()').extract()
             name = city_node.xpath('td[2]/a/text()').extract()
             city_url = city_node.xpath('td[1]/a/@href').extract()
             city_data['id'] = number[0]
             city_data['aname'] = name[0]
             city_data['lv'] = 2
-            city_data['pid'] = 0
+            city_data['pid'] = meta['id']
             country_url = self.baseUrl + str(city_url[0])
-            # yield scrapy.Request(country_url, meta={'city_data': city_data}, callback=self.country_parse,
-            #                      encoding='utf-8', dont_filter=True)
-            # 县
+            yield scrapy.Request(country_url, meta={'city_data': city_data}, callback=self.country_parse,
+                                 encoding='utf-8', dont_filter=True)
             yield city_data
 
 
@@ -53,23 +52,28 @@ class SpideroneSpider(scrapy.Spider):
         country_list = response.xpath('//tr[@class="countytr"]')
         for country_node in country_list:
 
-            if country_node.xpath('td/a/text()'):  # 此处if语句的判断耗时2小时，铭记。。
+            if country_node.xpath('td/a/text()'):
                 country_data = dict(meta)
                 number = country_node.xpath('td[1]/a/text()').extract()
                 name = country_node.xpath('td[2]/a/text()').extract()
-                country_data['countryNumber'] = number[0]
-                country_data['countryName'] = name[0]
+                country_data['id'] = number[0]
+                country_data['aname'] = name[0]
+                country_data['lv'] = 3
+                country_data['pid'] = meta['id']
                 country_url = country_node.xpath('td[1]/a/@href').extract()[0]
                 base_link = os.path.dirname(response.url)
                 next_link = '/'.join([base_link, country_url])
                 yield scrapy.Request(next_link, meta={'country_data': country_data}, callback=self.town_parse,
                                      encoding='utf-8', dont_filter=True)
+                yield country_data
             else:
                 country_data = dict(meta)
                 number = country_node.xpath('td[1]/text()').extract()
                 name = country_node.xpath('td[2]/text()').extract()
-                country_data['countryNumber'] = number[0]
-                country_data['countryName'] = name[0]
+                country_data['id'] = number[0]
+                country_data['aname'] = name[0]
+                country_data['lv'] = 3
+                country_data['pid'] = meta['id']
                 yield country_data
 
     def town_parse(self, response):
@@ -79,14 +83,16 @@ class SpideroneSpider(scrapy.Spider):
             town_data = dict(meta)
             number = town_node.xpath('td[1]/a/text()').extract()
             name = town_node.xpath('td[2]/a/text()').extract()
-            town_data['townNumber'] = number[0]
-            town_data['townName'] = name[0]
+            town_data['id'] = number[0]
+            town_data['aname'] = name[0]
+            town_data['lv'] = 4
+            town_data['pid'] = meta['id']
             town_url = town_node.xpath('td[1]/a/@href').extract()[0]
             base_link = os.path.dirname(response.url)
             next_link = '/'.join(([base_link, town_url]))
             yield scrapy.Request(next_link, meta={'town_data': town_data}, callback=self.village_parse,
                                  encoding='utf-8', dont_filter=True)
-
+            yield town_data
     def village_parse(self, response):
         meta = response.meta['town_data']
         village_list = response.xpath('//tr[@class="villagetr"]')
@@ -95,7 +101,9 @@ class SpideroneSpider(scrapy.Spider):
             number = village_node.xpath('td[1]/text()').extract()
             code = village_node.xpath('td[2]/text()').extract()
             name = village_node.xpath('td[3]/text()').extract()
-            village_data['villageNumber'] = number[0]
-            village_data['villageCode'] = code[0]
-            village_data['villageName'] = name[0]
+            village_data['id'] = number[0]
+            # village_data['villageCode'] = code[0]
+            village_data['aname'] = name[0]
+            village_data['lv'] = 5
+            village_data['pid'] = meta['id']
             yield village_data
